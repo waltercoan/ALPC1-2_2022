a0dfc9f07663c7c21cd00ac06abd659e3301173b
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-08-19 22:17:46 -0300

    19/08/2022

---
+++ percdespesa.py
@@ -0,0 +1,7 @@
+print("Digite o valor do salario")
+#float = numeros decimais
+valsalario = float(input())
+print("Digite o valor da despesa")
+valdespesa = float(input())
+percdespesa = (valdespesa * 100) / valsalario
+print("O percentual da despesa e", percdespesa)
---
+++ revelacao.py
@@ -0,0 +1,11 @@
+#ENTRADA
+#esse comando imprime na tela
+print("Digite a quantidade de fotos reveladas")
+#variavel
+#input() - le dados digitados pelo usuario
+#int() - converte texto pra numero
+numfotos = int(input())
+#PROCESSAMENTO
+valortotal = numfotos * 0.9
+#SAIDA
+print("O valor total e", valortotal)
