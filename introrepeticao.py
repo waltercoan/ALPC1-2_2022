a6a89c1c015c52e380dea232c16987b872cc37d7
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-10-28 22:03:06 -0300

    blocos de repetição

---
+++ introrepeticao.py
@@ -0,0 +1,15 @@
+#import time
+#while True:
+#    print("Rodando... ")
+#    time.sleep(0.001)
+
+contador = 0
+while contador < 10:
+    print(contador)
+    contador = contador + 1
+
+#aqui...9,8,7,6,5,4,3,2,1,0
+contador = 9
+while contador >= 0:
+    print(contador)
+    contador = contador - 1
