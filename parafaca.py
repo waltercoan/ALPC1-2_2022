077b94652a89bf8671360680977b1b76786f7fd4
WALTER SILVESTRE COAN <walter.s@univille.br>
2022-11-18 22:03:15 -0300

    introdução ao bloco de repetição for

---
+++ parafaca.py
@@ -0,0 +1,6 @@
+#print(list(range(0,10)))
+
+for cont in range(10):
+    print(cont)
+    if cont > 5:
+        break
---
+++ parafaca1exerc1.py
@@ -0,0 +1,10 @@
+'''
+Calcule e apresente a PROGRESSÃO GEOMÉTRICA dos 200
+próximos números de 1 considerando o quociente 4.
+'''
+prog = 1
+for cont in range(200):
+    print(prog, end="-")
+    prog = prog * 4
+#aquiohhhh
+print("O resultado e ", prog) 
